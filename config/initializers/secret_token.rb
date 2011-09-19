fedb2ed08d18af21c1736ede7290393e32a508e4
unknown <AmolJ@.USA.hardie.win>
2011-09-18 20:58:03 -0700

    Creating Framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vyadhi::Application.config.secret_token = '34789bf02cdacffa6614ac1922965f0af13a059b78010758f16870909f33045d560d7444045da55c99aaf7745a868d81d4268b08768623d227fe17651f8f19b8'
