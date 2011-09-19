f66880fbdaf3d1a09daa1a86576aa9a3ed37265d
unknown <AmolJ@.USA.hardie.win>
2011-09-18 23:05:30 -0700

    static pages.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def terms
+  end
+
+  def disclaimer
+  end
+
+  def privacy_policy
+  end
+
+  def investor_relations
+  end
+
+end
