33dbb900400237d9e9cb9565e59fafcb59711bff
unknown <AmolJ@.USA.hardie.win>
2011-10-17 21:47:54 -0700

    anychages

--- db/migrate/20111004114849_fix_columns_boolean_in_diseases2.rb
+++ db/migrate/20111004114849_fix_columns_boolean_in_diseases2.rb
@@ -1,9 +1,14 @@
 class FixColumnsBooleanInDiseases2 < ActiveRecord::Migration
   def self.up
   	rename_column :diseases, :condom?, :IsCondom
+  	rename_column :diseases, :symptoms?, :IsSymptoms
+  	rename_column :diseases, :curable?, :IsCurable
+  	
   end
 
   def self.down
   	rename_column :diseases, :IsCondom, :condom?
+  	rename_column :diseases, :IsCurable, :curable?
+  	rename_column :diseases, :IsSymptoms, :symptoms?
   end
 end
