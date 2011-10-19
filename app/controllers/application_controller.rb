7d4342349e92bf37b6d37e1b2fb3653120b7ff1e
unknown <AmolJ@.USA.hardie.win>
2011-10-19 00:57:58 -0700

    updated views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery
   layout 'standard' #added by Amol.
+  
+  	rescue_from CanCan::AccessDenied do |exception|
+       flash[:error] = exception.message
+       flash[:error] = "Limited access."
+       redirect_to root_url
+    end
+  	
+    rescue_from ActiveRecord::RecordNotFound do |exception|    
+  	flash[:error] = exception.message    
+  	klass = params[:controller].classify    
+  	url = url_for(:controller => params[:controller])    
+  	request.env["HTTP_REFERER"] ||= url    
+  	redirect_to :back  
+  end
 end
